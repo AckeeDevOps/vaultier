54b19ec449a00e7e789cbbb82ba120cc0b78cbc6
Stepan Vrany <stepan.vrany@ackee.cz>
2019-01-15 09:26:00 +0100

    add documentation

--- specs.go
+++ specs.go
@@ -1,22 +1,34 @@
 package main
 
-type secretKeyMapEntry struct {
+/*
+SecretKeyMapEntry represents details about one KEY:VALUE secret pair
+VaultKey represent name of the key under data are stored in Vault
+LocalKey represents name of the local key e.g. KEY becomes results['KEY']
+*/
+type SecretKeyMapEntry struct {
 	VaultKey string `yaml:"vaultKey"`
 	LocalKey string `yaml:"localKey"`
 }
 
-type secretPapthEntry struct {
-	Path   string              `yaml:"path"`
-	KeyMap []secretKeyMapEntry `yaml:"keymap"`
-}
-
-type specsEntry struct {
-	Branch  string             `yaml:"branch"`
-	Secrets []secretPapthEntry `yaml:"secrets"`
-}
-
-// Specs is representation of input YAML specification
 /*
+SecretPathEntry represents Vault path e.g. secret/data/keys
+KeyMap allows to retreive multiple keys from the single path
+*/
+type SecretPathEntry struct {
+	Path   string              `yaml:"path"`
+	KeyMap []SecretKeyMapEntry `yaml:"keymap"`
+}
+
+/*
+SpecsEntry represents configuration for the certain git branch
+*/
+type SpecsEntry struct {
+	Branch  string            `yaml:"branch"`
+	Secrets []SecretPathEntry `yaml:"secrets"`
+}
+
+/*
+Specs is representation of input YAML specification
 example:
 ---
 vaultAddr: ''
@@ -32,5 +44,5 @@ specs:
 type Specs struct {
 	VaultAddr string       `yaml:"vaultAddr"` // optional
 	Token     string       `yaml:"token"`     // optional, don't do that
-	Specs     []specsEntry `yaml:"specs"`     // required
+	Specs     []SpecsEntry `yaml:"specs"`     // required
 }
