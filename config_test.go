4f70142ce5b2a449a3d3a681c2ad1c43d62ea89e
Stepan Vrany <vrany.stepan@gmail.com>
2019-01-19 19:50:49 +0100

    first test

--- config_test.go
+++ config_test.go
@@ -1,7 +1,24 @@
 package main
 
-log = New(func(string) {})
+import (
+	"os"
+	"testing"
 
-func TestTimeConsuming(t *testing.T) {
-	log.Fatal("bla")
+	"github.com/vranystepan/vaultier/config"
+)
+
+func TestValidConfiguration(t *testing.T) {
+	os.Setenv("PLUGIN_VAULT_ADDR", "http://vault.co.uk")
+	os.Setenv("PLUGIN_VAULT_TOKEN", "abcdefg")
+	os.Setenv("PLUGIN_BRANCH", "master")
+	os.Setenv("PLUGIN_RUN_CAUSE", "delivery")
+	os.Setenv("PLUGIN_OUTPUT_FORMAT", "helm")
+	os.Setenv("PLUGIN_SECRET_SPECS_PATH", "/tmp/input")
+	os.Setenv("PLUGIN_SECRET_OUTPUT_PATH", "/tmp/output")
+
+	cfg := config.Create()
+	err := cfg.Validate()
+	if err != nil {
+		t.Errorf("Validator should be happy but it return %s", err)
+	}
 }
