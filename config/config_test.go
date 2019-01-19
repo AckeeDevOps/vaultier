882e51180c4ecc5ca595093b49aebeb14ab21885
Stepan Vrany <vrany.stepan@gmail.com>
2019-01-19 22:31:54 +0100

    add more tests

--- config/config_test.go
+++ config/config_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestValidConfiguration(t *testing.T) {
+func setEnv() {
 	os.Setenv("PLUGIN_VAULT_ADDR", "http://vault.co.uk")
 	os.Setenv("PLUGIN_VAULT_TOKEN", "abcdefg")
 	os.Setenv("PLUGIN_BRANCH", "master")
@@ -13,7 +13,10 @@ func TestValidConfiguration(t *testing.T) {
 	os.Setenv("PLUGIN_OUTPUT_FORMAT", "helm")
 	os.Setenv("PLUGIN_SECRET_SPECS_PATH", "/tmp/input")
 	os.Setenv("PLUGIN_SECRET_OUTPUT_PATH", "/tmp/output")
+}
 
+func TestValidConfiguration(t *testing.T) {
+	setEnv()
 	cfg := Create()
 	err := cfg.Validate()
 	if err != nil {
@@ -22,13 +25,8 @@ func TestValidConfiguration(t *testing.T) {
 }
 
 func TestValidConfigurationWithoutInput(t *testing.T) {
-	os.Setenv("PLUGIN_VAULT_ADDR", "http://vault.co.uk")
-	os.Setenv("PLUGIN_VAULT_TOKEN", "abcdefg")
-	os.Setenv("PLUGIN_BRANCH", "master")
-	os.Setenv("PLUGIN_RUN_CAUSE", "delivery")
-	os.Setenv("PLUGIN_OUTPUT_FORMAT", "helm")
-	os.Setenv("PLUGIN_SECRET_SPECS_PATH", "") // <= here
-	os.Setenv("PLUGIN_SECRET_OUTPUT_PATH", "/tmp/output")
+	setEnv()
+	os.Setenv("PLUGIN_SECRET_SPECS_PATH", "")
 
 	cfg := Create()
 	err := cfg.Validate()
@@ -38,13 +36,19 @@ func TestValidConfigurationWithoutInput(t *testing.T) {
 }
 
 func TestInvalidConfigurationWithoutAddress(t *testing.T) {
-	os.Setenv("PLUGIN_VAULT_ADDR", "") // <= here
-	os.Setenv("PLUGIN_VAULT_TOKEN", "abcdefg")
-	os.Setenv("PLUGIN_BRANCH", "master")
-	os.Setenv("PLUGIN_RUN_CAUSE", "delivery")
-	os.Setenv("PLUGIN_OUTPUT_FORMAT", "helm")
-	os.Setenv("PLUGIN_SECRET_SPECS_PATH", "/tmp/input")
-	os.Setenv("PLUGIN_SECRET_OUTPUT_PATH", "/tmp/output")
+	setEnv()
+	os.Setenv("PLUGIN_VAULT_ADDR", "")
+
+	cfg := Create()
+	err := cfg.Validate()
+	if err == nil {
+		t.Errorf("Validator should return error but it does not")
+	}
+}
+
+func TestInvalidConfigurationWithoutToken(t *testing.T) {
+	setEnv()
+	os.Setenv("PLUGIN_VAULT_TOKEN", "") // <= here
 
 	cfg := Create()
 	err := cfg.Validate()
