3ac573ea9667260c05019125e1aa7b1a80960e73
Stepan Vrany <stepan.vrany@ackee.cz>
2019-03-30 11:21:44 +0100

    add documentation

--- client/vault_response.go
+++ client/vault_response.go
@@ -1,9 +1,11 @@
 package client
 
+// VaultReponseData represents the actual secrets stored in Vault
 type VaultResponseData struct {
 	Data map[string]interface{} `json:"data"`
 }
 
+// VaultResponseMetadata represens secret's metadata
 type VaultResponseMetadata struct {
 	CreatedTime  string `json:"created_time"`
 	DeletionTime string `json:"deletion_time"`
@@ -11,6 +13,7 @@ type VaultResponseMetadata struct {
 	Version      int    `json:"version"`
 }
 
+// VaultResponse represents the actual Vault response
 type VaultResponse struct {
 	RequestID     string            `json:"request_id"`
 	LeaseID       string            `json:"lease_id"`
